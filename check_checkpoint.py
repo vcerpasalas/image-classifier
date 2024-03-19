b8b309347b18c0b4fc1b744fa571ec5e6dd6fdca
Valeria Cerpa Salas <46113798+vcerpasalas@users.noreply.github.com>
2024-03-19 18:54:48 -0500

    Delete check_checkpoint.py

--- check_checkpoint.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-def check_checkpoint(filepath):
-    checkpoint = torch.load(filepath)
-    print("Keys in the checkpoint:")
-    print(checkpoint.keys())
-
-checkpoint_file = "C:\\Users\\vcerp\\Downloads\\CODIGOS\\ImageClassifier\\checkpoint.pth"
-check_checkpoint(checkpoint_file)
